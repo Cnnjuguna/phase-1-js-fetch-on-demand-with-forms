7440fc317159b8b6f9fe39ef614035d39c1b8953
njugudev <njugudev@pop-os.localdomain>
2023-06-26 11:48:02 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 const init = () => {
-  
+
+  const inputForm = document.querySelector('form');
+
+  inputForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const inputField = document.querySelector('input#searchByID');
+    const id = inputField.value;
+    console.log(id);
+
+
+            const response = await fetch(`http://localhost:3000/movies/${id}`);
+
+            const data = await response.json()
+          
+
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+
+        title.textContent = data.title;
+        summary.textContent = data.summary;
+
+        
+    });
+        
+
+
 }
+  
+
 
 document.addEventListener('DOMContentLoaded', init);
